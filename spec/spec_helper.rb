05e71b9b1be99441d0d0fc0428e4d8f55a5cf602
Peer Rails <peer@omck.ws>
2016-12-05 16:18:42 +0300

    fix test database connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'simplecov'
-ENV["DATABASE_URL"] = "postgres://test:test@localhost/vedabot_test"
+ENV["DATABASE_URL"] ||= "postgres://test:test@localhost/vedabot_test"
 SimpleCov.start do
 	add_filter "spec/vcr_test.rb"
 end
